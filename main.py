dc9488ccec16df6958b98d89c3dec4ea59c8a81a
Ray <105521700+Barrow75@users.noreply.github.com>
2023-08-01 08:02:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import sys
 import matplotlib
 import numpy as np
 
+# inputs represent the input nodes of the Neural Network
 inputs = [1.2, 5.1, 2.1]
+# weiights that hold corresponding weight values
 weights = [3.1, 2.1, 8.7]
+
 bias = 3
 
 output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
 print(output)
+
+
